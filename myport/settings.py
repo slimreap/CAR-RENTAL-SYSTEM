96849fb0c0cf0b654ef1a5688cbbba84da33ae0f
shrewded <120327934+shrewded@users.noreply.github.com>
2022-12-13 12:40:02 +0800

    FINAL SYS

--- myport/settings.py
+++ myport/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
