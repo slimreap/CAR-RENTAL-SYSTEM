d4ff9704797e715f5816dfee453f69beab1fc5eb
shrewded <120327934+shrewded@users.noreply.github.com>
2022-12-13 11:55:46 +0800

    FINALS

--- myport/settings.py
+++ myport/settings.py
@@ -129,5 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
-    BASE_DIR/ 'static',
+    os.path.join(BASE_DIR, 'static')
 ]
